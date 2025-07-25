eef12fedbfcea0aef59ba57c564e061a528e1ead
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-07-25 20:53:04 -0300

    feat: create header

---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import avatar from '../assets/avatar.webp';
+</script>
+
+<template>
+  <header class="h-screen max-w-[1920px] mx-auto min-h-200 sm:min-h-160 md:min-h-152 lg:min-h-0 flex flex-col justify-center items-center bg-primary-background text-primary-text">
+    <div class="w-full p-6 flex flex-col justify-center items-center gap-5 lg:flex-row lg:p-0 lg:gap-0 lg:h-full">
+      <section class="w-full flex justify-center items-center lg:bg-primary-title-background lg:h-full lg:w-3/10">
+        <figure>
+          <img class="rounded-full w-38 md:w-50 xl:w-60" :src="avatar" alt="Higor Moroni" />
+        </figure>
+      </section>
+
+      <section class="w-full flex flex-col lg:h-full lg:w-7/10 lg:p-12">
+        <div class="w-full flex flex-col gap-5 items-center lg:h-full lg:justify-center lg:items-start lg:gap-10 xl:gap-6">
+          <h1 class="flex flex-col items-center text-5xl/14 lg:items-start xl:text-7xl/20">
+            <span class="font-light">Higor</span>
+            <span class="text-highlight-text uppercase font-bold">Moroni</span>
+          </h1>
+  
+          <h2 class="font-light xl:text-2xl">Desenvolvedor Front-end III</h2>
+  
+          <div class="w-full flex flex-col gap-1 items-center text-2xl lg:items-start">
+            <ul class="flex gap-2">
+              <li>
+                <a href="#">
+                  <span class="fi fi-br rounded"></span>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="grayscale hover:grayscale-0 transition-all duration-500">
+                  <span class="fi fi-us rounded"></span>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="grayscale hover:grayscale-0 transition-all duration-500">
+                  <span class="fi fi-es rounded"></span>
+                </a>
+              </li>
+            </ul>
+            <div class="w-full flex justify-between items-center">
+              <ul class="flex gap-2">
+                <li>
+                  <a>
+                    <ion-icon name="sunny-outline" class="active" />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <ion-icon name="moon-outline" />
+                  </a>
+                </li>
+              </ul>
+              <ul class="flex gap-2">
+                <li>
+                  <a href="https://github.com/HigorMoroni" target="_blank">
+                    <ion-icon name="logo-github" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/higormoroni/" target="_blank">
+                    <ion-icon name="logo-linkedin" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/moroni.higor" target="_blank">
+                    <ion-icon name="logo-instagram" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+  
+        <div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center items-center gap-7 border-t-2 border-line pt-7">
+          <div class="flex flex-col gap-3 items-center lg:items-start">
+            <strong>Cidade Atual</strong>
+            <span>Santos, SP</span>
+          </div>
+          <div class="flex flex-col gap-3 items-center lg:items-start">
+            <strong>Telefone</strong>
+            <a>(13) 98868-5814</a>
+          </div>
+          <div class="flex flex-col gap-3 items-center lg:items-start">
+            <strong>Site</strong>
+            <a>www.higor.dev</a>
+          </div>
+          <div class="flex flex-col gap-3 items-center lg:items-start">
+            <strong>E-mail</strong>
+            <a>moroni@higor.dev</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </header>
+</template>
