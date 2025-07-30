0f3b393670ca7fde23775ac05d3f4d930ea8c3bf
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-07-30 18:04:31 -0300

    feat: create skills section

---
+++ src/components/Skills.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DefaultLayout from '@/layouts/default.vue';
+</script>
+
+<template>
+  <DefaultLayout>
+    <template #title>
+    </template>
+
+    <template #content>
+    </template>
+  </DefaultLayout>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import About from '@/components/About.vue';
 import Header from '@/components/Header.vue';
+import Skills from '@/components/Skills.vue';
 </script>
 
 <template>
   <Header />
   <About />
+  <Skills />
 </template>
