735ab2cd21bc677fa36f409b7cbab53e55647cb0
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-07-30 16:54:56 -0300

    feat: create default layout

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
+import DefaultLayout from '@/layouts/default.vue';
 </script>
 
 <template>
-  <section class="flex flex-col lg:flex-row">
-    <div class="p-6 flex flex-col gap-2 border-y border-line bg-mobile-title-background md:p-11 lg:border-none lg:text-right lg:w-3/10 lg:bg-secondary-title-background">
+  <DefaultLayout>
+    <template #title>
       <h2 class="uppercase font-bold text-base">Sobre mim</h2>
       <p>Apenas um resumo.</p>
-    </div>
-    <div class="p-6 flex flex-col gap-3 md:p-11 lg:w-7/10 lg:bg-secondary-page-background">
-      <p>Analista Desenvolvedor de Sistemas, graduado em 2019 pela Faculdade de Tecnologia do Estado de São Paulo em Praia Grande, São Paulo. Enquanto acadêmico, atuei por 6 meses como monitor da disciplina de Algoritmos e Lógica de Programação, onde pude auxiliar os novos alunos a pensar logicamente e conhecer os conceitos fundamentais do desenvolvendo. Sou uma pessoa enérgica e bem focada, dedicada ao autoaperfeiçoamento; eu me concentro em aprender e aplicar habilidades de maneira rápida e eficiente, se houver algo para aprender e colocar em prática no processo de uma empresa, eu o faço; minha intenção é sempre dar um mergulho produtivo na empresa em que trabalho. Encontro-me estudando tecnologias JavaScript para desenvolvimento Full-stack, além de práticas de desenvolvendo web e mobile.</p>
-      <p class="text-highlight-text font-bold">Atualmente estou em busca de uma oportunidade como Desenvolvedor Júnior ou Trainee para me aperfeiçoar e iniciar minha carreira profissional.</p>
-      <p>Meu desejo é aplicar meus conhecimentos para a resolução de problemas e ajudar na entrega de projetos com alta qualidade, além de desenvolver tais conhecimentos ao obter mais experiência. Gostaria de trabalhar com metodologias ágeis e entender o ciclo de um projeto empresarial.</p>
-    </div>
-  </section>
+    </template>
+
+    <template #content>
+      <p>
+        Analista Desenvolvedor de Sistemas, graduado em 2019 pela Faculdade de Tecnologia do Estado de São Paulo em Praia Grande, São Paulo. Enquanto acadêmico, atuei por 6 meses como monitor da disciplina de Algoritmos e Lógica de Programação, onde pude auxiliar os novos alunos a pensar logicamente e conhecer os conceitos fundamentais do desenvolvendo. Sou uma pessoa enérgica e bem focada, dedicada ao autoaperfeiçoamento; eu me concentro em aprender e aplicar habilidades de maneira rápida e eficiente, se houver algo para aprender e colocar em prática no processo de uma empresa, eu o faço; minha intenção é sempre dar um mergulho produtivo na empresa em que trabalho. Encontro-me estudando tecnologias JavaScript para desenvolvimento Full-stack, além de práticas de desenvolvendo web e mobile.
+      </p>
+      <p class="text-highlight-text font-bold">
+        Atualmente estou em busca de uma oportunidade como Desenvolvedor Júnior ou Trainee para me aperfeiçoar e iniciar minha carreira profissional.
+      </p>
+      <p>
+        Meu desejo é aplicar meus conhecimentos para a resolução de problemas e ajudar na entrega de projetos com alta qualidade, além de desenvolver tais conhecimentos ao obter mais experiência. Gostaria de trabalhar com metodologias ágeis e entender o ciclo de um projeto empresarial.
+      </p>
+    </template>
+  </DefaultLayout>
 </template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <section class="w-full flex flex-col lg:flex-row">
+    <slot>
+      <div class="p-6 flex flex-col gap-2 border-y border-line bg-mobile-title-background md:p-11 lg:border-none lg:text-right lg:w-3/10 lg:bg-secondary-title-background">
+        <slot name="title" />
+      </div>
+      <div class="p-6 flex flex-col gap-3 md:p-11 lg:w-7/10 lg:bg-secondary-page-background">
+        <slot name="content" />
+      </div>
+    </slot>
+  </section>
+</template>
