e3a0b7f437898e350add2cac73b63047364b33d6
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-08-01 18:17:34 -0300

    style: fix education and experience spacing

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,25 +3,25 @@ import DefaultLayout from '@/layouts/default.vue';
 </script>
 
 <template>
-  <DefaultLayout variation="secondary" hide-top-padding>
+  <DefaultLayout hide-top-padding>
     <template #title>
       <h3>Formação</h3>
       <p>Onde aprendi a aprender.</p>
     </template>
 
     <template #content>
-      <ol class="flex flex-col border-l-2 border-line pl-6 pt-6 ml-3 relative">
+      <ol class="flex flex-col border-l-2 border-line pl-6 py-8 ml-3 relative gap-8 md:py-12">
         <div class="border-line h-[1px] w-4 absolute border -left-2.25 bottom-0"></div>
-        <li v-for="n in 2" class="relative mb-6">
+        <li v-for="n in 2" class="relative">
           <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-[0_0_0_8px_#fff]" />
-          <div>
-            <div class="flex flex-col mb-4 gap-1">
+          <div class="flex flex-col gap-1 md:flex-row md:gap-12">
+            <div class="md:w-4/10">
               <span class="text-xs">2015 - 2019</span>
               <h4>Faculdade de Tecnologia do Estado de São Paulo</h4>
               <h5 class="font-medium text-sm text-secondary-text">Graduação, Análise e Desenvolvimento de Sistemas</h5>
               <span class="text-xs">Praia Grande, SP</span>
             </div>
-            <p>
+            <p class="md:w-6/10">
               Monitor Acadêmico: Algoritmos e Lógica de Programação;
               TCC: BateVolta - Sistema de Gerenciamento de Viagens;
               Participação em eventos acadêmicos e palestras;
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -10,17 +10,17 @@ import DefaultLayout from '@/layouts/default.vue';
     </template>
 
     <template #content>
-      <ol class="flex flex-col border-l-2 border-line pl-6 ml-3 relative">
-        <li v-for="n in 4" class="relative mb-6">
+      <ol class="flex flex-col border-l-2 border-line pl-6 pb-8 ml-3 relative gap-8 md:pb-12 mb:gap-12 xl:pb-16 xl:gap-16">
+        <li v-for="n in 4" class="relative">
           <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-[0_0_0_8px_#fff]" />
-          <div>
-            <div class="flex flex-col mb-4 gap-1">
+          <div class="flex flex-col gap-1 md:flex-row md:gap-12">
+            <div class="md:w-4/10">
               <span class="text-xs">2017 - Presente</span>
               <h4>Prefeitura Municipal da Praia Grande</h4>
               <h5 class="font-medium text-sm text-secondary-text">Agente Administrativo</h5>
               <span class="text-xs">Praia Grande, SP</span>
             </div>
-            <p>
+            <p class="md:w-6/10">
               Gerenciamento de rotina e atividades administrativas de média complexidade; resolução de problemas comuns ao exercício das atividades escolares; soluções básicas e intermediárias com Excel e VBA; controle e expedição de documentos e processos de acordo com os padrões de redação oficial; atendimento ao público e organização.
             </p>
           </div>
