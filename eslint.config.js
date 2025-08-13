d9620dc36a89f252bf31f7130cbf54554b28c24d
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-08-13 19:59:04 -0300

    fix: add new lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,18 @@ export default antfu({
   rules: {
     'style/semi': ['error', 'always'],
     'style/comma-dangle': ['error', 'never'],
-    'vue/comma-dangle': ['error', 'never']
+    'vue/comma-dangle': ['error', 'never'],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: 1
+    }],
+    'vue/first-attribute-linebreak': ['error', {
+      singleline: 'ignore',
+      multiline: 'below'
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always'
+    }]
   }
 });
