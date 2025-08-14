751cd3355c52876abecc25072b612c53c0f98ac6
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-08-14 01:24:09 -0300

    feat: use courses dynamic data

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,28 +1,17 @@
 <script setup lang="ts">
+import { useStore } from '@/composables/useStore';
 import DefaultLayout from '@/layouts/default.vue';
 
-const courses = [
-  { name: 'AWS', img: 'aws' },
-  { name: 'Laravel', img: 'laravel' },
-  { name: 'NoSQL', img: 'nosql' }
-];
+const { courses } = useStore();
 </script>
 
 <template>
-  <DefaultLayout variation="secondary">
-    <template #title>
-      <h3>Cursos em Andamento</h3>
-      <p>Em breve desenvolvendo muito mais.</p>
-    </template>
-
+  <DefaultLayout variation="secondary" :title="courses.title" :subtitle="courses.subtitle">
     <template #content>
       <ul class="flex gap-6 flex-wrap">
-        <li
-          v-for="course in courses"
-          :key="course.name"
-        >
+        <li v-for="course in courses.items" :key="course.name">
           <img
-            :src="course.img"
+            :src="course.image"
             :alt="course.name"
             :title="course.name"
             class="w- transition-all duration-1000 ease-in-out grayscale hover:grayscale-0"
