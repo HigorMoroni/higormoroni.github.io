18f34b38e286333e98a4bf71e8cb6fd742eef85a
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-08-14 01:31:56 -0300

    feat: use contact dynamic data

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,52 +1,28 @@
 <script setup lang="ts">
+import { useStore } from '@/composables/useStore';
 import DefaultLayout from '@/layouts/default.vue';
+
+const { contact, personal, socials } = useStore();
 </script>
 
 <template>
-  <DefaultLayout>
-    <template #title>
-      <h3>Contato</h3>
-      <p>Me chama.</p>
-    </template>
-
+  <DefaultLayout :title="contact.title" :subtitle="contact.subtitle">
     <template #content>
       <div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7 border-b-2 border-line pb-7">
-        <div class="flex flex-col gap-3">
-          <strong>Cidade Atual</strong>
-          <span>Santos, SP</span>
-        </div>
-        <div class="flex flex-col gap-3">
-          <strong>Telefone</strong>
-          <a>(13) 98868-5814</a>
-        </div>
-        <div class="flex flex-col gap-3">
-          <strong>Site</strong>
-          <a>www.higor.dev</a>
-        </div>
-        <div class="flex flex-col gap-3">
-          <strong>E-mail</strong>
-          <a>moroni@higor.dev</a>
+        <div v-for="item in contact.items" :key="item.title" class="flex flex-col gap-3">
+          <strong>{{ item.title }}</strong>
+          <span>{{ item.content }}</span>
         </div>
       </div>
       <div class="flex justify-between mt-16 sm:mt-24 xl:mt-32">
         <h1 class="flex flex-col text-5xl/14">
-          <span class="font-light">Higor</span>
-          <span class="text-highlight uppercase font-bold">Moroni</span>
+          <span class="font-light">{{ personal.firstName }}</span>
+          <span class="text-highlight uppercase font-bold">{{ personal.lastName }}</span>
         </h1>
         <ul class="self-end hidden gap-2 text-2xl lg:flex">
-          <li>
-            <a href="https://github.com/HigorMoroni" target="_blank">
-              <ion-icon name="logo-github" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/higormoroni/" target="_blank">
-              <ion-icon name="logo-linkedin" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/moroni.higor" target="_blank">
-              <ion-icon name="logo-instagram" />
+          <li v-for="social in socials.items" :key="social.title">
+            <a :href="social.url" target="_blank">
+              <ion-icon :name="`logo-${social.title.toLowerCase()}`" />
             </a>
           </li>
         </ul>
