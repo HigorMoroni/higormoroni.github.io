6d7f495d0c41d02be753b7cceb260252dac20d20
Higor Moroni <higor.ferreira@melhorenvio.com>
2025-08-15 18:22:09 -0300

    feat: create dark theme

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -11,7 +11,7 @@ const { education } = useStore();
       <ol class="flex flex-col border-l-2 border-line pl-6 py-8 ml-3 relative gap-8 md:py-12">
         <div class="border-line h-[1px] w-4 absolute border -left-2.25 bottom-0" />
         <li v-for="item in education.items" :key="item.degree" class="relative">
-          <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-[0_0_0_8px_#fff]" />
+          <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-ring" />
           <div class="flex flex-col gap-1 md:flex-row md:gap-12">
             <div class="md:w-4/10">
               <span class="text-xs">{{ item.period }}</span>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -15,7 +15,7 @@ const { experience } = useStore();
     <template #content>
       <ol class="flex flex-col border-l-2 border-line pl-6 pb-8 ml-3 relative gap-8 md:pb-12 mb:gap-12 xl:pb-16 xl:gap-16">
         <li v-for="item in experience.items" :key="item.company" class="relative">
-          <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-[0_0_0_8px_#fcfcfc]" />
+          <div class="bg-highlight h-2.5 w-2.5 absolute -left-7.5 top-2 rounded-full shadow-ring shadow-ring--secondary" />
           <div class="flex flex-col gap-1 md:flex-row md:gap-12">
             <div class="md:w-4/10">
               <span class="text-xs">{{ `${item.startDate} - ${item.endDate}` }}</span>
